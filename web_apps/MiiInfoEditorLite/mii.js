b056302f599fa7c86513544f93e371445b92161f
Kazuki <akki2001.ex@gmail.com>
2020-06-03 00:18:37 +0900

    MiiInfoEditorLiteの追加

---
+++ web_apps/MiiInfoEditorLite/mii.js
@@ -0,0 +1,122 @@
+const MII_FILE_SIZE = 0x4A;
+const MII_NAME_LENGTH = 10;
+const ID_LENGTH = 0x4;
+const UNEDIT1_SIZE = 2;
+const UNEDIT2_SIZE = 0x14;
+var editMii = {
+	//0x0
+	invalid:false,
+    isGirl:false,
+    month:0,
+	day:0,
+    favColor:0,
+	isFavorite:false,
+	//0x2
+	name:'',
+	//0x16
+    height:0x40,
+	//0x17
+	weight:0x40,
+	//0x18
+    miiID:[0x86,0xb4,0x03,0xf0],
+	//0x1C
+    consoleID:[0xec,0xff,0x82,0xd2],
+	//0x20
+	unEdit1:[0x00,0x00],
+	mingleOff:true,
+	//0x22
+　　unEdit2:[0x42,0x40,0x31,0xbd,0x28,0xa2,0x08,0x8c,0x08,0x40,0x14,0x49,0xb8,0x8d,0x00,0x8a,0x00,0x8a,0x25,0x04,0x00],
+	//0x36
+	creatorName:''
+};
+
+function getBoolean(int){
+	if(int === 1)return true;
+	return false;
+}
+
+function getInt(boolean){
+	if(boolean === true)return 1;
+	return 0;
+}
+
+function bufToUtf16String(buf){
+	var tmpU16,i,string;
+	string = '';
+	for(i = 0;i < MII_NAME_LENGTH;i++){
+		tmpU16 = buf[i * 2] * 256 + buf[i * 2 + 1];
+		if(tmpU16 === 0)break;
+		string += String.fromCharCode(tmpU16);
+	}
+	return string;
+}
+
+function miiFileRead(buf){
+	console.log(buf);
+	editMii.invalid = getBoolean(buf[0] >>> 7);
+	editMii.isGirl = getBoolean((buf[0] >>> 6) & 1);
+	editMii.month = (buf[0] >>> 2) &　0xf;
+	editMii.day = ((buf[0] & 3) << 3) + (buf[1] >>> 5);
+	editMii.favColor = (buf[1] >>> 1) & 0xf;
+	editMii.isFavorite = getBoolean(buf[1] & 1);
+	var i;
+	var stringBuf = [MII_NAME_LENGTH * 2];
+	for(i = 0;i < MII_NAME_LENGTH;i++){
+		stringBuf[i * 2] = buf[i * 2 + 0x2];
+		stringBuf[i * 2 + 1] = buf[i * 2 + 0x2 + 1];
+	}
+	editMii.name = bufToUtf16String(stringBuf);
+	editMii.height = buf[0x16];
+	editMii.weight = buf[0x17];
+	for(i = 0;i < ID_LENGTH;i++){
+		editMii.miiID[i] = buf[0x18 + i];
+		editMii.consoleID[i] = buf[0x1C + i];
+	}
+	editMii.unEdit1[0] = buf[0x20];
+	editMii.mingleOff = getBoolean((buf[0x21] >>> 2) & 1);
+	editMii.unEdit1[1] = buf[0x21] & 0xfb;
+	for(i = 0;i < UNEDIT2_SIZE;i++){
+        editMii.unEdit2[i] = buf[0x22 + i];
+	}
+	for(i = 0;i < MII_NAME_LENGTH;i++){
+		stringBuf[i * 2] = buf[i * 2 + 0x36];
+		stringBuf[i * 2 + 1] = buf[i * 2 + 0x36 + 1];
+	}
+	editMii.creatorName = bufToUtf16String(stringBuf);
+	setUI();
+}
+
+function miiFileWrite(){
+	var i;
+	var buf = new Uint8Array(MII_FILE_SIZE);
+	buf[0] = (getInt(editMii.invalid) << 7) + (getInt(editMii.isGirl) << 6) + (editMii.day >>> 3) + (editMii.month << 2);
+	buf[1] = ((editMii.day & 7) << 5) + (editMii.favColor << 1) + getInt(editMii.isFavorite);
+	for(i = 0;i < MII_NAME_LENGTH;i++){
+        if(editMii.name.length <= i){
+			buf[2 + i * 2] = 0;
+			buf[2 + i * 2 + 1] = 0;
+		}else{
+			buf[2 + i * 2] = Math.floor(editMii.name.charCodeAt(i) / 256);
+			buf[2 + i * 2 + 1] = editMii.name.charCodeAt(i) % 256;
+		}
+		if(editMii.creatorName.length <= i){
+			buf[0x36 + i * 2] = 0;
+			buf[0x36 + i * 2 + 1] = 0;
+		}else{
+			buf[0x36 + i * 2] = Math.floor(editMii.creatorName.charCodeAt(i) / 256);
+			buf[0x36 + i * 2 + 1] = editMii.creatorName.charCodeAt(i) % 256;
+		}
+	}
+	buf[0x16] = editMii.height;
+	buf[0x17] = editMii.weight;
+	for(i = 0;i < ID_LENGTH;i++){
+		buf[0x18 + i] = editMii.miiID[i];
+		buf[0x1C + i] = editMii.consoleID[i];
+	}
+	buf[0x20] = editMii.unEdit1[0];
+	buf[0x21] = editMii.unEdit1[1] + (getInt(editMii.mingleOff) << 2);
+	for(i = 0;i < UNEDIT2_SIZE;i++){
+        buf[0x22 + i] = editMii.unEdit2[i];
+	}
+	return buf;
+}
---
+++ web_apps/MiiInfoEditorLite/script.js
@@ -0,0 +1,376 @@
+var i;
+var reader = new FileReader();
+
+function sendEdit(){
+}
+//DOMの取得
+var open = document.getElementById('open');
+var save = document.getElementById('save');
+var miiName = document.getElementById('miiName');
+var creatorName = document.getElementById('creatorName');
+var height = document.getElementById('height');
+var weight = document.getElementById('weight');
+var heightNum = document.getElementById('heightNum');
+var weightNum = document.getElementById('weightNum');
+var month = document.getElementById('month');
+var day = document.getElementById('day');
+var gender = document.getElementById('gender');
+var mingles = document.getElementById('mingles');
+var favorite = document.getElementById('favorite');
+var miiId = [ID_LENGTH];
+var consoleId = [ID_LENGTH];
+var colorButton = [12];
+for(i = 0;i < ID_LENGTH;i++){
+    miiId[i] = document.getElementById('m' + i);
+    consoleId[i] = document.getElementById('c' + i);
+}
+
+for(i = 0;i < 12;i++){
+    colorButton[i] = document.getElementById('colorButton' + i);
+}
+//DOMにイベント追加
+
+for(i = 0;i < ID_LENGTH;i++){
+    miiId[i].addEventListener('change',(event) => {
+        var id = Number(event.target.getAttribute('id').replace('m',''));
+        var tmp = stoh(event.target.value);
+        if(tmp < 0 || tmp > 255 || (tmp !== 0 && !tmp)){
+            setUI();
+            return;
+        }
+        editMii.miiID[id] = tmp;
+        setUI();
+        sendEdit();
+    });
+    consoleId[i].addEventListener('change',(event) => {
+        var id = Number(event.target.getAttribute('id').replace('c',''));
+        var tmp = stoh(event.target.value);
+        if(tmp < 0 || tmp > 255 || (tmp !== 0 && !tmp)){
+            setUI();
+            return;
+        }
+        editMii.consoleID[id] = tmp;
+        setUI();
+        sendEdit();
+    });
+}
+
+gender.addEventListener('click',(event) => {
+    if(editMii.isGirl){
+        editMii.isGirl = false;
+    }else{
+        editMii.isGirl = true;
+    }
+    setUI();
+    sendEdit();
+});
+
+mingles.addEventListener('click',(event) => {
+    if(editMii.mingleOff){
+        editMii.mingleOff = false;
+    }else{
+        editMii.mingleOff = true;
+    }
+    setUI();
+    sendEdit();
+});
+
+favorite.addEventListener('click',(event) => {
+    if(editMii.isFavorite){
+        editMii.isFavorite = false;
+    }else{
+        editMii.isFavorite = true;
+    }
+    setUI();
+    sendEdit();
+});
+
+miiName.addEventListener('change',(event) => {
+    editMii.name = miiName.value;
+    setUI();
+    sendEdit();
+});
+
+creatorName.addEventListener('change',(event) => {
+    editMii.creatorName = creatorName.value;
+    setUI();
+    sendEdit();
+});
+
+height.addEventListener('change',(event) => {
+    editMii.height = height.value;
+    setUI();
+    sendEdit();
+});
+
+heightNum.addEventListener('change',(event) => {
+    var tmp = ston(heightNum.value);
+    if(tmp < 0 || tmp > 127 || (tmp !== 0 && !tmp)){
+        setUI();
+        return;
+    }
+    editMii.height = tmp;
+    setUI();
+    sendEdit();
+});
+
+weight.addEventListener('change',(event) => {
+    editMii.weight = weight.value;
+    setUI();
+    sendEdit();
+});
+
+weightNum.addEventListener('change',(event) => {
+    var tmp = ston(weightNum.value);
+    if(tmp < 0 || tmp > 127 || (tmp !== 0 && !tmp)){
+        setUI();
+        return;
+    }
+    editMii.weight = tmp;
+    setUI();
+    sendEdit();
+});
+
+for(i = 0;i < 12;i++){
+    colorButton[i].addEventListener('click',(event) => {
+        editMii.favColor = Number(event.target.getAttribute('id').replace('colorButton',''));
+        setUI();
+        sendEdit();
+    });
+}
+month.addEventListener('change',(event) => {
+    var tmp = ston(month.value);
+    if(tmp < 0 || tmp > 12 || (tmp !== 0 && !tmp)){
+        setUI();
+        return;
+    }
+    editMii.month = tmp;
+    setUI();
+    sendEdit();
+});
+
+day.addEventListener('change',(event) => {
+    var tmp = ston(day.value);
+    if(tmp < 0 || tmp > 31 || (tmp !== 0 && !tmp)){
+        setUI();
+        return;
+    }
+    editMii.day = tmp;
+    setUI();
+    sendEdit();
+});
+//空白削除
+function deleteSpace(string){
+    return string.replace(/\s+/g, "");
+}
+//文字列を10進数に変換
+function ston(a){
+    var string = deleteSpace(a);
+    var i,dec,num = 0;
+    for(i = 0;i < string.length;i++){
+        dec = getDec(string.charAt(i));
+        if(dec === -1)return null;
+        num = num * 10 + dec;
+    }
+    return num;
+}
+
+function getDec(char){
+    switch(char){
+        case '0':
+        case '０':
+            return 0;
+        case '1':
+        case '１':
+            return 1;
+        case '2':
+        case '２':
+            return 2;
+        case '3':
+        case '３':
+            return 3;
+        case '4':
+        case '４':
+            return 4;
+        case '5':
+        case '５':
+            return 5;
+        case '6':
+        case '６':
+            return 6;
+        case '7':
+        case '７':
+            return 7;
+        case '8':
+        case '８':
+            return 8;
+        case '9':
+        case '９':
+            return 9;
+        default:
+            return -1;
+    }
+}
+//文字列を16進数に変換
+function stoh(a){
+    var string = deleteSpace(a);
+    var i,hex,num = 0;
+    for(i = 0;i < string.length;i++){
+        hex = getHex(string.charAt(i));
+        if(hex === -1)return null;
+        num = num * 16 + hex;
+    }
+    return num;
+}
+function getHex(char){
+    switch(char){
+        case '0':
+        case '０':
+            return 0;
+        case '1':
+        case '１':
+            return 1;
+        case '2':
+        case '２':
+            return 2;
+        case '3':
+        case '３':
+            return 3;
+        case '4':
+        case '４':
+            return 4;
+        case '5':
+        case '５':
+            return 5;
+        case '6':
+        case '６':
+            return 6;
+        case '7':
+        case '７':
+            return 7;
+        case '8':
+        case '８':
+            return 8;
+        case '9':
+        case '９':
+            return 9;
+        case 'a':
+        case 'A':
+        case 'ａ':
+        case 'Ａ':
+            return 10;
+        case 'b':
+        case 'B':
+        case 'ｂ':
+        case 'Ｂ':
+            return 11;
+        case 'c':
+        case 'C':
+        case 'ｃ':
+        case 'Ｃ':
+            return 12;
+        case 'd':
+        case 'D':
+        case 'ｄ':
+        case 'Ｄ':
+            return 13;
+        case 'e':
+        case 'E':
+        case 'ｅ':
+        case 'Ｅ':
+            return 14;
+        case 'f':
+        case 'F':
+        case 'ｆ':
+        case 'Ｆ':
+            return 15;
+        default:
+            return -1;
+    }
+}
+
+function getString(int){
+    var str = int.toString(16);
+    if(str.length < 2)str = '0' + str;
+    return str;
+}
+
+function setUI(){
+    miiName.value = editMii.name;
+    creatorName.value = editMii.creatorName;
+    for(i = 0;i < ID_LENGTH;i++){
+        miiId[i].value = getString(editMii.miiID[i]);
+        consoleId[i].value = getString(editMii.consoleID[i]);
+    }
+    height.value = editMii.height;
+    heightNum.value = editMii.height;
+    weight.value = editMii.weight;
+    weightNum.value = editMii.weight;
+    if(editMii.month !== 0){
+        month.value = editMii.month;
+    }else{
+        month.value = '';
+    }
+    if(editMii.day !== 0){
+        day.value = editMii.day;
+    }else{
+        day.value = '';
+    }
+    if(editMii.isGirl){
+        gender.innerHTML = 'Female';
+    }else{
+        gender.innerHTML = 'Male';
+    }
+    if(editMii.mingleOff){
+        mingles.innerHTML = 'No';
+    }else{
+        mingles.innerHTML = 'Yes';
+    }
+    if(editMii.isFavorite){
+        favorite.innerHTML = '<img src=\"img/favorite.png\">';
+    }else{
+        favorite.innerHTML = '<img src=\"img/none.png\">';
+    }
+    for(i = 0;i < 12;i++){
+        if(i === editMii.favColor){
+            colorButton[i].setAttribute('class','selectColor');
+        }else{
+            colorButton[i].setAttribute('class','notSelectColor');
+        }
+    }
+};
+
+function fileCheck(){
+    var buf = new Uint8Array(reader.result);
+    if(buf.length === MII_FILE_SIZE){
+        miiFileRead(buf);
+        return;
+    }else if(buf.length > 0x88){
+        if(buf[0] === 0x52 && buf[1] === 0x4B && buf[2] === 0x47 && buf[3] === 0x44){
+            miiFileRead(buf.subarray(0x3C,0x3C + MII_FILE_SIZE));
+            return;
+        }
+    }
+    open.value = '';
+    alert("invalid file");
+}
+
+reader.addEventListener('load',fileCheck,false);
+open.addEventListener('change',loadFile,false);
+function loadFile(event){
+    var tmp = event.target.files;
+    if(tmp){
+        var f = tmp[0];
+        reader.readAsArrayBuffer(f);
+    }
+}
+
+save.addEventListener('click',function(event){
+    var blob = new Blob([miiFileWrite()]);
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'default.miigx'
+    link.click();
+});
+
+setUI();
